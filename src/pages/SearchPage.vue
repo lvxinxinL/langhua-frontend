a07a6cea18f7e2c91e6366896ac4fbc43f2f0348
Ghost <2089049079@qq.com>
2024-01-09 18:39:37 +0800

    add 搜索标签过滤功能 add 取消搜索功能

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,7 +1,7 @@
 <template>
   <form action="/">
     <van-search
-        v-model="value"
+        v-model="searchText"
         show-action
         placeholder="请输入要搜索的标签"
         @search="onSearch"
@@ -10,17 +10,18 @@
   </form>
 
   <van-divider content-position="left">已选标签</van-divider>
-
-  <van-tag v-for="tag in activeIds" :show="true" closeable size="medium" type="primary">
-    {{ tag }}
-  </van-tag>
-
+  <div v-if="activeIds.length===0">请选择标签</div>
+  <van-space wrap>
+    <van-tag v-for="tag in activeIds" :show="true" closeable size="medium" type="primary" @close="doClose(tag)">
+      {{ tag }}
+    </van-tag>
+  </van-space>
   <van-divider content-position="left">可选标签</van-divider>
 
   <van-tree-select
       v-model:active-id="activeIds"
       v-model:main-active-index="activeIndex"
-      :items="tags"
+      :items="tagList"
   />
 
 </template>
@@ -29,31 +30,82 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const value = ref('');
-const onSearch = (val) => alert(val);
-const onCancel = () => alert('取消');
+const searchText = ref('');
 
-// 已选中的标签
-const activeIds = ref([]);
-const activeIndex = ref(0);
-const tags = [
+/**
+ * 标签列表（从后端获取）
+ */
+const originalTagList = [
   {
     text: '性别',
     children: [
-      { text: '男', id: 1 },
-      { text: '女', id: 2 },
+      { text: '男', id: '男' },
+      { text: '女', id: '女' },
     ],
   },
   {
     text: '学习方向',
     children: [
-      { text: 'Java', id: 4 },
-      { text: 'C++', id: 5 },
-      { text: 'Python', id: 6 },
+      { text: 'Java', id: 'Java' },
+      { text: 'C++', id: 'C++' },
+      { text: 'Python', id: 'Python' },
     ],
   },
-  { text: '福建', disabled: true },
+  {
+    text: '本科生',
+    children: [
+      { text: '大一', id: '大一' },
+      { text: '大二', id: '大二' },
+      { text: '大三', id: '大三' },
+      { text: '大四', id: '大四' },
+    ],
+  },
+  {
+    text: '研究生',
+    children: [
+      { text: '研一', id: '研一' },
+      { text: '研二', id: '研二' },
+    ],
+  },
 ];
+
+let tagList = ref(originalTagList);
+
+/**
+ * 搜索标签过滤
+ * @param val
+ */
+const onSearch = (val) => {
+  tagList.value = originalTagList.map(parentTag => {
+    const tempChildren = [...parentTag.children];
+    const tempParentTag = {...parentTag};
+    tempParentTag.children = tempChildren.filter(item => item.text.includes(searchText.value));
+    return tempParentTag;
+  });
+};
+
+/**
+ * 取消搜索
+ */
+const onCancel = () => {
+  searchText.value='';
+  tagList.value=originalTagList;
+};
+
+// 已选中的标签
+const activeIds = ref([]);
+const activeIndex = ref(0);
+
+
+/**
+ * 关闭标签
+ * @param tag 要关闭的标签 id
+ */
+const doClose = (tag) => {
+  activeIds.value = activeIds.value.filter(item => {
+    return item !== tag;
+  })
+}
 
 </script>
 
